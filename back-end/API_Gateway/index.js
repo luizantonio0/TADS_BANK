c0bfc2a09895c05343b6a9ca308ee6cf6f2af8cb
victorbetoni <victorbetoni@protonmail.com>
2026-03-19 16:50:05 -0300

    Tirando os .env, só vai rodar local mesmo

--- back-end/API_Gateway/index.js
+++ back-end/API_Gateway/index.js
@@ -3,13 +3,11 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-const MS_AUTH_URL = process.env.MS_AUTH_URL;
-
+const PORT = 3000;
 
 const services = {
-    ms_auth: process.env.MS_AUTH_URL,
-    ms_cliente: process.env.MS_CLIENTE_URL,
+    ms_auth: "http://tadsbank:8055",
+    ms_cliente: "http://localhost:8080"
 };
 
 app.use(async (req, res, next) => {
@@ -18,7 +16,7 @@ app.use(async (req, res, next) => {
     }
     try {
         const token = req.headers['authorization'];
-        const authResp = await fetch(`${MS_AUTH_URL}/auth/validate`, {
+        const authResp = await fetch(`${services.ms_auth}/auth/validate`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -54,5 +52,5 @@ app.use('/ms_auth', createProxyMiddleware({
 
 
 app.listen(PORT, () => {
-    console.log(`API Gateway rodando em http://localhost:${PORT}`);
+    console.log(`API Gateway rodando em http://tadsbank:${PORT}`);
 });
