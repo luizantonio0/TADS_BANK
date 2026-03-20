2b5aa3598c18b064f41cd3646eb756d9ba7189b0
luizantonio0 <antonio.luiz@ufpr.br>
2026-03-20 16:50:15 -0300

    server mockado para facilitar teste do gateway

---
+++ back-end/API_Gateway/mock_server.js
@@ -0,0 +1,127 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+const porta = 3001;
+
+let dados;
+
+try {
+    const dadosBrutos = fs.readFileSync(path.join(__dirname, 'mock.json'), 'utf8');
+    dados = JSON.parse(dadosBrutos);
+} catch (erro) {
+    console.error("Erro ao ler o arquivo:", erro);
+}
+
+
+
+app.get('/clientes', (req, res) => {
+    return res.status(200).json(
+        dados.GET.clientes
+    )
+})
+app.get('/clientes/:cpf', (req, res) => {
+    return res.status(200).json(
+        dados.GET.clientes
+    )
+})
+
+app.post('/clientes', (req, res) => {
+    return res.status(200).json(
+        dados.POST.clientes.default
+    )
+})
+app.post('/clientes/:cpf/aprovar', (req, res) => {
+    return res.status(200).json(
+        dados.POST.clientes.aprovar
+    )
+})
+
+app.post('/clientes/:cpf/rejeitar', (req, res) => {
+    return res.status(200).json(
+        dados.POST.clientes.rejeitar
+    )
+})
+
+app.put('/clientes', (req, res) => {
+    return res.status(200).json(
+        dados.PUT.clientes
+    )
+})
+
+// =============================== CONTAS =================================
+
+app.get('/contas/:numero/saldo', (req, res) => {
+    return res.status(200).json(
+        dados.GET.contas.default
+    )
+})
+app.get('/contas/:numero/extrato', (req, res) => {
+    return res.status(200).json(
+        dados.GET.contas.extrato
+    )
+})
+
+app.post('/contas/:numero/depositar', (req, res) => {
+    return res.status(200).json(
+        dados.POST.contas.depositar
+    )
+})
+
+app.post('/contas/:numero/sacar', (req, res) => {
+    return res.status(200).json(
+        dados.POST.contas.sacar
+    )
+})
+
+app.post('/contas/:numero/transferir', (req, res) => {
+    return res.status(200).json(
+        dados.POST.contas.transferir
+    )
+})
+
+// ========================= GERENTE =========================
+
+app.get('/gerentes', (req, res) => {
+    return res.status(200).json(
+        dados.GET.gerentes
+    )
+})
+app.get('/gerentes/:cpf', (req, res) => {
+    return res.status(200).json(
+        dados.GET.gerentes
+    )
+})
+
+app.post('/gerentes', (req, res) => {
+    return res.status(200).json(
+        dados.POST.gerentes
+    )
+})
+app.put('/gerentes/:cpf', (req, res) => {
+    return res.status(200).json(
+        dados.PUT.gerentes
+    )
+})
+
+app.delete('/gerentes/:cpf', (req, res) => {
+    return res.status(200).json(
+        dados.DELETE.gerentes
+    )
+})
+
+// LOGIN
+
+app.post('/login', (req, res) => {
+    return res.status(200).json(
+        dados.POST.login
+    )
+})
+
+app.post('/logout', (req, res) => {
+    return res.status(200).json(
+        dados.POST.logout
+    )
+})
+
+app.listen(porta, () => console.log(`Servidor rodando em http://localhost:${porta}`));
