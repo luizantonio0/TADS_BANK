b8b6754af3705b050283d4f6c56e59e759691c19
Spirou1 <enzospironellipinheiro@gmail.com>
2026-03-29 16:24:48 -0300

    adicionando modal p excluir gerente

--- front-end/src/app/pages/administrador/tab-manter-gerentes/tab-manter-gerentes.ts
+++ front-end/src/app/pages/administrador/tab-manter-gerentes/tab-manter-gerentes.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { ModalEditarGerente } from '../modal-editar-gerente/modal-editar-gerente';
+import { ModalExcluirGerente } from '../modal-excluir-gerente/modal-excluir-gerente';
 
 @Component({
   selector: 'tab-manter-gerentes',
-  imports: [ModalEditarGerente],
+  imports: [ModalEditarGerente, ModalExcluirGerente],
   templateUrl: './tab-manter-gerentes.html',
   styleUrl: './tab-manter-gerentes.css',
 })
 export class TabManterGerentes {
 
   exibirModalEditar: boolean = false;
+  exibirModalExcluir: boolean = false;
 
   abrirModalEdicao() {
     this.exibirModalEditar = true;
   }
 
+  abrirModalExcluir() {
+    this.exibirModalExcluir = true;
+  }
 }
