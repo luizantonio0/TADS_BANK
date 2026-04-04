a93b74b47251830a8ec418feee82c40fbbf34630
Spirou1 <enzospironellipinheiro@gmail.com>
2026-04-04 00:07:29 -0300

    integrando front p/ gerente

--- front-end/src/app/pages/administrador/tab-manter-gerentes/tab-manter-gerentes.ts
+++ front-end/src/app/pages/administrador/tab-manter-gerentes/tab-manter-gerentes.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ModalEditarGerente } from '../modal-editar-gerente/modal-editar-gerente';
 import { ModalExcluirGerente } from '../modal-excluir-gerente/modal-excluir-gerente';
 import { ModalAdicionarGerente } from '../modal-adicionar-gerente/modal-adicionar-gerente';
+import { GerenteService } from '../../../shared/service/requests/gerente';
+import { GerenteResponse } from '../../../shared/models/gerente.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'tab-manter-gerentes',
@@ -9,11 +12,29 @@ import { ModalAdicionarGerente } from '../modal-adicionar-gerente/modal-adiciona
   templateUrl: './tab-manter-gerentes.html',
   styleUrl: './tab-manter-gerentes.css',
 })
-export class TabManterGerentes {
+export class TabManterGerentes implements OnInit {
+
+  gerenteService = inject(GerenteService);
+  gerentes: GerenteResponse[] = [];
 
   exibirModalEditar: boolean = false;
   exibirModalExcluir: boolean = false;
   exibirModalAdicionar: boolean = false;
+
+  ngOnInit(): void {
+    this.carregarGerentes();
+  }
+
+  carregarGerentes() {
+    this.gerenteService.getGerentes().subscribe({
+      next: (dados) => {
+        this.gerentes = dados;
+      },
+      error: (erro) => {
+        console.error('Erro ao carregar gerentes:', erro);
+      }
+    });
+  }
 
   abrirModalEdicao() {
     this.exibirModalEditar = true;
