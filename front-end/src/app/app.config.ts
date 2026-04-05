383e6e8e4a6bda57aa6a6de3c27860602398ba41
luizantonio0 <antonio.luiz@ufpr.br>
2026-04-05 02:19:45 -0300

    integração do front-end com o gateway (carregar cliente ao realizar login)

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, LOCALE_ID } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideNgxMask } from 'ngx-mask';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
@@ -12,7 +12,7 @@ registerLocaleData(localePt);
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideNgxMask(),
     provideHttpClient(),
     { provide: LOCALE_ID, useValue: 'pt-BR' },
