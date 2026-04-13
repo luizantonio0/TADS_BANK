0bc7e2208310f3c12a98682802592ad7488a378f
luizantonio0 <antonio.luiz@ufpr.br>
2026-04-13 19:25:49 -0300

    Criação MS_Gerente

---
+++ back-end/MS_gerente/src/test/java/MS_Gerente/bantads/BantadsApplicationTests.java
@@ -0,0 +1,13 @@
+package MS_Gerente.bantads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BantadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
