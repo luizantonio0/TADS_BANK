7a691abaea1d970cc8d98d3a2d8f47d4fb4562ce
Spirou1 <enzospironellipinheiro@gmail.com>
2026-05-26 21:07:56 -0300

    arrumando conflitos

--- back-end/MS_conta/src/main/java/com/bantads/conta/service/ContaService.java
+++ back-end/MS_conta/src/main/java/com/bantads/conta/service/ContaService.java
@@ -90,7 +90,6 @@ public class ContaService {
         return contaRepository.findByCpfIn(cpf);
     }
 
-<<<<<<< HEAD
     @Transactional(readOnly = true)
     public List<Conta> findByContaNum(List<String> contas) {
         return contaRepository.findByCpfIn(contas);
@@ -98,9 +97,6 @@ public class ContaService {
 
 
     @Transactional(readOnly = true)
-=======
-    @Transactional
->>>>>>> b1a4c2eeb2797e27829411f2235a01a7d5cfd850
     public Conta getConta(String numConta) {
         return contaRepository.findByConta(numConta)
                 .orElseThrow(() -> new IllegalArgumentException("Conta não encontrada"));
